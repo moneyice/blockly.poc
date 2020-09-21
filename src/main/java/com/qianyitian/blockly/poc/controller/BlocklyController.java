7b85f86948c07977062d8a4dea79980f26451348
bing.a.qian <bing.a.qian@acc>
2020-09-21 11:36:59 +0800

    functionUtil

--- src/main/java/com/qianyitian/blockly/poc/controller/BlocklyController.java
+++ src/main/java/com/qianyitian/blockly/poc/controller/BlocklyController.java
@@ -3,8 +3,11 @@ package com.qianyitian.blockly.poc.controller;
 import com.googlecode.aviator.AviatorEvaluator;
 import com.googlecode.aviator.Expression;
 import com.googlecode.aviator.runtime.JavaMethodReflectionFunctionMissing;
+import com.googlecode.aviator.runtime.function.AbstractFunction;
 import com.qianyitian.blockly.function.MonthPerformFactorFunction;
 import com.qianyitian.blockly.function.MonthPerformSalaryStandardFunction;
+import com.qianyitian.blockly.util.FileUtil;
+import com.qianyitian.blockly.util.FunctionUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.*;
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -40,35 +44,19 @@ public class BlocklyController {
 
     }
 
-//    @PostMapping(value = "/run")
-//    public String run(@RequestBody String input) throws IOException {
-//        logger.info("Running   ======================================= " + input);
-//        Map<String, Object> env = AviatorEvaluator.newEnv("userID", 1001L, "baseTax", 10000D);
-//        AviatorEvaluator.addFunction(new AddFunction());
-//        AviatorEvaluator.setFunctionMissing(JavaMethodReflectionFunctionMissing.getInstance());
-//        Expression exp = AviatorEvaluator.getInstance().compileScript(rule_file_path, false);
-//        Object execute = exp.execute(env);
-//        BigDecimal tax = (BigDecimal) execute;
-//        env = AviatorEvaluator.newEnv("userID", 1002L, "baseTax", 10000D);
-//        execute = exp.execute(env);
-//        tax = (BigDecimal) execute;
-//        System.out.println("运行结果为： " + tax);
-//        return String.valueOf(tax);
-//    }
-
 
     @PostMapping(value = "/saveScript/{ruleName}")
     public String saveScript(@PathVariable String ruleName, @RequestBody String input) throws IOException {
         logger.info("saveScript   ======================================= " + input);
         String filename = rule_folder_path + ruleName + ".av";
-        fileLinesWrite(filename, input);
+        FileUtil.fileLinesWrite(filename, input);
         return "OK";
     }
 
     @PostMapping(value = "/loadScript/{ruleName}")
     public String loadScript(@PathVariable String ruleName) throws IOException {
         String filename = rule_folder_path + ruleName + ".av";
-        String s = ReadFile(filename);
+        String s = FileUtil.readFile(filename);
         logger.info("loadScript   ======================================= " + s);
         return s;
     }
@@ -77,14 +65,14 @@ public class BlocklyController {
     public String saveXML(@PathVariable String ruleName, @RequestBody String input) throws IOException {
         logger.info("saveXML   ======================================= " + input);
         String filename = xml_folder_path + ruleName + ".xml";
-        fileLinesWrite(filename, input);
+        FileUtil.fileLinesWrite(filename, input);
         return "OK";
     }
 
     @PostMapping(value = "/loadXML/{ruleName}")
     public String loadXML(@PathVariable String ruleName) throws IOException {
         String filename = xml_folder_path + ruleName + ".xml";
-        String content = ReadFile(filename);
+        String content = FileUtil.readFile(filename);
         logger.info("loadXML   ======================================= " + content);
         return content;
     }
@@ -98,11 +86,14 @@ public class BlocklyController {
         }
         String userid = input;
         Map<String, Object> env = AviatorEvaluator.newEnv("userId", userid);
-        AviatorEvaluator.addFunction(new MonthPerformFactorFunction());
-        AviatorEvaluator.addFunction(new MonthPerformSalaryStandardFunction());
 
-        AviatorEvaluator.setFunctionMissing(JavaMethodReflectionFunctionMissing.getInstance());
-
+        List<AbstractFunction> list= FunctionUtil.findAllFunctions();
+        for (AbstractFunction abstractFunction : list) {
+            AviatorEvaluator.addFunction(abstractFunction);
+        }
+//        AviatorEvaluator.addFunction(new MonthPerformSalaryStandardFunction());
+//
+//        AviatorEvaluator.setFunctionMissing(JavaMethodReflectionFunctionMissing.getInstance());
         Expression exp = AviatorEvaluator.getInstance().compileScript(filename, false);
         Object execute = exp.execute(env);
         BigDecimal tax = (BigDecimal) execute;
@@ -112,65 +103,8 @@ public class BlocklyController {
     }
 
 
-    public static String ReadFile(String Path) {
-        BufferedReader reader = null;
-        String laststr = "";
-        try {
-            FileInputStream fileInputStream = new FileInputStream(Path);
-            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
-            reader = new BufferedReader(inputStreamReader);
-            String tempString = null;
-            while ((tempString = reader.readLine()) != null) {
-                laststr += tempString;
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return laststr;
-    }
 
-    /**
-     * 文件数据写入（如果文件夹和文件不存在，则先创建，再写入）
-     *
-     * @param filePath
-     * @param content
-     */
-    public static void fileLinesWrite(String filePath, String content) {
-        FileWriter fw = null;
-        try {
-            File file = new File(filePath);
-            //如果文件夹不存在，则创建文件夹
-            if (!file.getParentFile().exists()) {
-                file.getParentFile().mkdirs();
-            }
-            if (!file.exists()) {//如果文件不存在，则创建文件,写入第一行内容
-                file.createNewFile();
-                fw = new FileWriter(file);
-            } else {//如果文件存在,则追加或替换内容
-                fw = new FileWriter(file, false);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        PrintWriter pw = new PrintWriter(fw);
-        pw.println(content);
-        pw.flush();
-        try {
-            fw.flush();
-            pw.close();
-            fw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+
+
 
 }
---
+++ src/main/java/com/qianyitian/blockly/util/FileUtil.java
@@ -0,0 +1,69 @@
+package com.qianyitian.blockly.util;
+
+import java.io.*;
+
+public class FileUtil {
+
+
+    /**
+     * 文件数据写入（如果文件夹和文件不存在，则先创建，再写入）
+     *
+     * @param filePath
+     * @param content
+     */
+    public static void fileLinesWrite(String filePath, String content) {
+        FileWriter fw = null;
+        try {
+            File file = new File(filePath);
+            //如果文件夹不存在，则创建文件夹
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+            if (!file.exists()) {//如果文件不存在，则创建文件,写入第一行内容
+                file.createNewFile();
+                fw = new FileWriter(file);
+            } else {//如果文件存在,则追加或替换内容
+                fw = new FileWriter(file, false);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PrintWriter pw = new PrintWriter(fw);
+        pw.println(content);
+        pw.flush();
+        try {
+            fw.flush();
+            pw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static String readFile(String Path) {
+        BufferedReader reader = null;
+        String laststr = "";
+        try {
+            FileInputStream fileInputStream = new FileInputStream(Path);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
+            reader = new BufferedReader(inputStreamReader);
+            String tempString = null;
+            while ((tempString = reader.readLine()) != null) {
+                laststr += tempString;
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return laststr;
+    }
+}
